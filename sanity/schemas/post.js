5b77b0c69483a279715bca13ef3240b41ce50a59
Arash Hosseini <arashhosseini733@gmail.com>
2022-11-23 11:47:40 +0330

    Implementing Next.js SSR

--- sanity/schemas/post.js
+++ sanity/schemas/post.js
@@ -6,7 +6,13 @@ export default {
     {
       name: 'title',
       title: 'Title',
+      description:'keep titles short!',
       type: 'string',
+    },
+    {
+      name: 'description',
+      title:'Description',
+      type:'string',
     },
     {
       name: 'slug',
